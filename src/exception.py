b9a897e6e907afd0210304e2081815b7c7affadf
Pratik date <pratikdate100@gmail.com>
2023-03-11 20:29:10 +0530

    logger

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-
+from logger import logging
 def error_message_detail(error,error_details):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename 
@@ -8,10 +8,19 @@ def error_message_detail(error,error_details):
     return error_message
 
 class custeomfixcetion(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self,error_message,error_details:sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+        self.error_message=error_message_detail(error_message,error_details=error_details)
 
 
     def __str__(self) :
+        
         return self.error_message
+    
+
+if __name__=="__main__":
+    try :
+            a=1/0
+    except Exception as e :
+            logging.info("devide by zero ")
+            raise custeomfixcetion(e,sys)
